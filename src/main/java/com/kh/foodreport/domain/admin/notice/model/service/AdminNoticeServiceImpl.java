cf3c4a09c7f37dadc3762c102ef1f951a78b5ff9
dltbxkfm86-creator <dltbxkfm86@gmial.com>
2026-02-11 10:24:27 +0900

    fixed:notice단로직수정

--- src/main/java/com/kh/foodreport/domain/admin/notice/model/service/AdminNoticeServiceImpl.java
+++ src/main/java/com/kh/foodreport/domain/admin/notice/model/service/AdminNoticeServiceImpl.java
@@ -49,89 +49,89 @@ public class AdminNoticeServiceImpl implements AdminNoticeService {
 
 		if (imgResult == 0) {
 			fileService.deleteStoredFile(imageUrl);
-		    throw new FileUploadException("이미지 저장 실패");
+			throw new FileUploadException("이미지 저장 실패");
 		}
 
 	}
-	
+
 	@Override
 	@Transactional
 	public void saveNotice(AdminNoticeDTO notice, MultipartFile file, Long memberNo) {
-		
+
 		notice.setRefMemberNo(memberNo);
-		
+
 		// 1. 공지사항을 먼저 저장
 		int result = noticeMapper.saveNotice(notice);
 
 		if (result == 0) { // 공지사항이 저장안됐을시(예외처리)
 			throw new NoticeCreationException("공지사항 등록에 실패하셨습니다!");
 		}
-		
+
 		// 공지사항 번호를 뽑음
 		Long num = notice.getNoticeNo();
 
 		// 2. 공지사항의 번호로 이미지가 있다면 notice에 이미지 정보 세팅
 		saveImage(file, num);
-		
+
 	}
 
 	@Override
 	@Transactional
 	public AdminNoticeResponse findAllNotices(int page) {
-		
+
 		GlobalValidator.validateNo(page, "0보다 작은 값은 들어갈 수 없습니다.");
-		
+
 		// 전체 개수 조회
 		int listCount = noticeMapper.countByNotices();
-				
+
 		Map<String, Object> pages = pagenation.getPageRequest(listCount, page, 10);
-		
+
 		List<AdminNoticeDTO> notices = noticeMapper.findAllNotices(pages);
-		
+
 		return createFindResponse(notices, pages);
 	}
-	
+
 	@Override
 	public AdminNoticeResponse findByNoticeTitle(int page, String noticeTitle) {
-		
-		if(noticeTitle == null || "".equals(noticeTitle.trim())) {
+
+		if (noticeTitle == null || "".equals(noticeTitle.trim())) {
 			throw new InvalidKeywordException("키워드를 입력해주세요.");
 		}
-		
+
 		// 부분 개수 조회
 		int listCount = noticeMapper.countByNoticeTitle(noticeTitle);
-		
+
 		Map<String, Object> pages = pagenation.getPageRequest(listCount, page, 10);
-		
+
 		pages.put("noticeTitle", noticeTitle);
-		
+
 		List<AdminNoticeDTO> notices = noticeMapper.findByNoticeTitle(pages);
-		
+
 		return createFindResponse(notices, pages);
 	}
-	
-	// 중복 메소드 분리 
+
+	// 중복 메소드 분리
 	private AdminNoticeResponse createFindResponse(List<AdminNoticeDTO> notices, Map<String, Object> pages) {
 		AdminNoticeResponse response = new AdminNoticeResponse();
-		
+
 		response.setAdminNotice(notices);
-		response.setPageInfo(((PageInfo)pages.get("pageInfo")));
+		response.setPageInfo(((PageInfo) pages.get("pageInfo")));
 		return response;
 	}
 
 	@Override
 	@Transactional
 	public void deleteNotice(Long noticeNo) {
-		
-		GlobalValidator.validateNo(noticeNo , "0보다 큰 값을 넣어주시길바랍니다.");
-		
+
+		GlobalValidator.validateNo(noticeNo, "0보다 큰 값을 넣어주시길바랍니다.");
+
 		noticeMapper.deleteNoticeImage(noticeNo);
-		
+
 		// 2. 다음 공지사항 테이블 접근 1행 반환시 잘반환됨, 0행 반환시 삭제가 안된거기때문에 예외발생
 		int deleteResult = noticeMapper.deleteNotice(noticeNo);
-		
+
 		// 이미 삭제됐거나, 없는 데이터
-		if(deleteResult == 0) {
+		if (deleteResult == 0) {
 			throw new BoardDeleteException("게시물 삭제에 실패하였습니다.");
 		}
 	}
@@ -141,23 +141,21 @@ public class AdminNoticeServiceImpl implements AdminNoticeService {
 	public void updateNotice(Long noticeNo, AdminNoticeDTO notice, MultipartFile file) {
 
 		GlobalValidator.validateNo(noticeNo, "0보다 큰값을 넣어주시길 바랍니다.");
-		
+
 		String url = noticeMapper.countByNoticeNo(noticeNo); // 기존 파일 url 조회
 		notice.setNoticeNo(noticeNo);
-		
+
 		int noticeResult = noticeMapper.updateNotice(notice);
-		if(noticeResult == 0) {
+		if (noticeResult == 0) {
 			throw new NoticeCreationException("존재하지 않는 공지사항이거나 수정에 실패했습니다.");
 		}
-		if(url != null && !"".equals(url)) { // 기존 파일이 있음
-			if(file != null && !file.isEmpty()) { // 새 파일이 존재함
+		if (file != null && !file.isEmpty()) { // 새 파일이 존재함
+			if (url != null && !"".equals(url)) { // 기존 파일이 있음
 				String imageUrl = fileService.store(file);
-				AdminNoticeImage image = AdminNoticeImage.builder()
-						.originName(file.getOriginalFilename())
-						.changeName(imageUrl)
-						.refNoticeNo(noticeNo).build();
+				AdminNoticeImage image = AdminNoticeImage.builder().originName(file.getOriginalFilename())
+						.changeName(imageUrl).refNoticeNo(noticeNo).build();
 				int imageResult = noticeMapper.updateImage(image);
-				if(imageResult > 0) {
+				if (imageResult > 0) {
 					fileService.deleteStoredFile(url);
 				} else {
 					fileService.deleteStoredFile(imageUrl);
@@ -165,12 +163,8 @@ public class AdminNoticeServiceImpl implements AdminNoticeService {
 				}
 			}
 		} else { // 기존 파일이 없음
-			if(file != null && !file.isEmpty()) { // 새 파일이 존재함
-				saveImage(file, noticeNo);
-			}
+			saveImage(file, noticeNo);
 		}
-
 	}
-	
-	
+
 }
